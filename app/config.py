b46acd21da53b78ffcbc7be72ab9b47edd713032
YoccoDante <hi.seresx@gmail.com>
2025-05-21 22:36:50 -0500

    added new line to config file

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 
 class Config:
-    ALLOWED_ORIGINS = getenv('ALLOWED_ORIGINS', '*')
-    API_VERSION = getenv('API_VERSION', 'v1')
-    SQLALCHEMY_DATABASE_URI = getenv('SQLALCHEMY_DATABASE_URI', '')
-    SECRET_KEY = getenv('SECRET_KEY', '')
+    ALLOWED_ORIGINS = getenv("ALLOWED_ORIGINS", "*")
+    API_VERSION = getenv("API_VERSION", "v1")
+    SQLALCHEMY_DATABASE_URI = getenv("SQLALCHEMY_DATABASE_URI", "")
+    SECRET_KEY = getenv("SECRET_KEY", "")
